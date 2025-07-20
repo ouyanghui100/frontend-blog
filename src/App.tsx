a478145c9d79d664b86043669acb308b09821e39
ouyanghui <2902897795@qq.com>
2025-07-20 17:49:32 +0800

    wip: 仓库初始化

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Test} from './views/Test/Test.tsx';
+const App: React.FC = () => {
+  // const {
+  //   token: { colorBgContainer, borderRadiusLG },
+  // } = theme.useToken();
+
+  return <Test />;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createStyleImportPlugin } from 'vite-plugin-style-import';
+import * as reactPlugin from 'vite-plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // 解决报The requested module 'react-is/index.js' does not provide an export named 'ForwardRef'的问题
+  optimizeDeps: {
+    include: [
+     'react-is',
+    ]
+  },
+  plugins: [
+     // 解决报The requested module 'react-is/index.js' does not provide an export named 'ForwardRef'的问题
+    reactPlugin,
+    react(),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: name => `antd/es/${name}/style/index`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#1890ff', // 主题颜色配置
+        },
+      },
+    },
+  },
+  base: '/',
+});
