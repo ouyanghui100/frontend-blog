a09c845503adddc25f02e92b13c535da7a609507
OuyangHui <2902897795@qq.com>
2025-07-28 10:59:11 +0800

    feat(样式整合): 新增index.css文件并引入Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 import '@/design/index.scss'
 import '@ant-design/v5-patch-for-react-19'
 
