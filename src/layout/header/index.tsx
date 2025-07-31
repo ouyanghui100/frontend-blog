d3704839a624c974954929cd8e0be0623d733106
OuyangHui <2902897795@qq.com>
2025-07-31 18:45:12 +0800

    refactor(样式整合): 更新index.css文件，调整Tailwind CSS导入方式并引入Ant Design重置样式

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -7,7 +7,7 @@ const LayoutHeader = () => {
   const { Header } = Layout
 
   return (
-    <Header className="flex h-auto flex-col justify-between !bg-[#fff]">
+    <Header className="flex h-[48px] flex-col justify-between !bg-[#fff]">
       <div className="flex h-full w-full justify-between px-3 py-0">
         <div className="flex items-center">
           <FoldTrigger />
