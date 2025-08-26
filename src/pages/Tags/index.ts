88c674c518e216855808206f94e6ca99d2fbb5ef
OuyangHui <2902897795@qq.com>
2025-08-26 17:11:41 +0800

    feat(标签页面): 添加标签页面及路由配置

---
+++ src/pages/Tags/index.ts
@@ -0,0 +1,5 @@
+const TagsPage: React.FC = () => {
+  return <div>'111111111111111111'<div>
+}
+
+export default TagsPage
---
+++ src/router/routes/tags.tsx
@@ -0,0 +1,34 @@
+import { lazy } from '@loadable/component'
+import LazyLoad from '@/components/LazyLoad'
+import { LayoutGuard } from '../guard'
+import type { RouteObject } from '../types'
+
+// Home route
+const TagsRoute: RouteObject = {
+  path: '/tags',
+  name: 'Tags',
+  element: <LayoutGuard />,
+  meta: {
+    title: '标签',
+    icon: 'home',
+    affix: true,
+    orderNo: 1,
+    hideChildrenInMenu: true,
+  },
+  children: [
+    {
+      path: '',
+      name: 'TagsPage',
+      element: LazyLoad(lazy(() => import('@/pages/Tags'))),
+      meta: {
+        title: '标签',
+        key: 'tags',
+        icon: 'home',
+        orderNo: 1,
+        hideMenu: true,
+      },
+    },
+  ],
+}
+
+export default TagsRoute
